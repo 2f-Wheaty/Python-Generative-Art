21431fccdc9520d80f63c2f727d5501e469f4412
Lil_Wheaty <59319679+2f-Wheaty@users.noreply.github.com>
2021-11-24 14:30:07 -0500

    Delete tst directory

--- tst/test_generate_title.py
+++
@@ -1,6 +0,0 @@
-from src.art_name_generator import generate_name
-
-
-def test_generate_title():
-    test_name = generate_name("Electric Violet", "San Marino")
-    print(f"Test name: {test_name}")
